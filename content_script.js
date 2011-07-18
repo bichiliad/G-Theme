5b1e372de95a2a4e8032a67ad173281bb8e5d202
Salem Hilal <salemhilal@gmail.com>
2011-07-17 23:16:24 -0400

    Added a background, new logo, and a few color tweaks.

--- content_script.js
+++ content_script.js
@@ -1,5 +1,6 @@
-//alert('content_script.js has been loaded successfully.');
-
-chrome.tabs.executeScript(null,
-	{code:"document.body.style.backgroundColor='red'"});
-document.write('<link rel="stylesheet" href="/stylesie.css" type="text/css">');
+var script = document.createElement('script');
+script.src = 'http://code.jquery.com/jquery-1.6.2.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+//This replaces the logo. Nya ha ha.
+$(".a-U-Pg-T img:first").attr('src', 'http://www.technoheads.org/g+/img/white_logo_final.png');
